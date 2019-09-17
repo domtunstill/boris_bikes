85095d829dd244970981905ccf58e98c4430bc45
Dom Tunstill <datunstill@gmail.com>
2019-09-17 16:53:26 +0100

    Adding test for methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,9 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 end
+
+describe DockingStation do
+  describe '.release_bike' do
+    it { is_expected.to respond_to(:working?) }
+  end
+end
