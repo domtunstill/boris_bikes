bc4124c81162b79e3de00f0453d4cc2b7982a673
Dom Tunstill <datunstill@gmail.com>
2019-09-20 12:34:20 +0100

    added full and empty methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,22 @@ class DockingStation
   end
 
   def release_bike
-    raise "No bikes available" if @bikes.empty?
+    raise "No bikes available" if empty?
     @bikes.pop
   end
 
  def dock_bike(bike)
-   raise "Docking station is full" if @bikes.count >= 20
+   raise "Docking station is full" if full?
    @bikes << bike
+ end
+
+ private
+ def full?
+   @bikes.count >= 20
+ end
+
+ def empty?
+   @bikes.count == 0
  end
 
 end
